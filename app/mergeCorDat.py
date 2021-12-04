f08680026c0fb69a5a1787c11de93563b5280344
silviotmalmeida <silviotmalmeida@gmail.com>
2021-12-04 12:16:34 -0300

    Ajustes gerais

--- app/mergeCorDat.py
+++ app/mergeCorDat.py
@@ -5,156 +5,201 @@ import os
 print('Iniciando...')
 
 # definindo a localização deste arquivo python
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+__location__ = os.path.realpath(os.path.join(
+    os.getcwd(), os.path.dirname(__file__)))
 
-# abrindo o arquivo CHESF para leitura
-fileIn = open(os.path.join(__location__, 'corCHESF.dat'))
+# tratando exceções
+try:
 
-# inicializando o array de cores
-colors = []
+    # abrindo o arquivo CHESF para leitura
+    fileIn = open(os.path.join(__location__, 'corCHESF.dat'))
 
-# inicializando o dicionário de cada cor
-color = {}
+    # inicializando o array de cores
+    colors = []
 
-# iterando sobre as linhas do arquivo
-for line in fileIn:
+    # inicializando o dicionário de cada cor
+    color = {}
 
-    # separando os dados em um array chave valor
-    data = line.split('=')
+    # iterando sobre as linhas do arquivo
+    for line in fileIn:
 
-    # se a chave for ID
-    if data[0].strip() == 'ID':
-        # registra no dicionário
-        color[data[0].strip()] = data[1].strip()
+        # separando os dados em um array chave valor
+        data = line.split('=')
 
-    # se a chave for ORDEM
-    elif data[0].strip() == 'ORDEM':
-        # registra no dicionário
-        color[data[0].strip()] = data[1].strip()
+        # se a chave for ID
+        if data[0].strip() == 'ID':
+            # registra no dicionário
+            color[data[0].strip()] = data[1].strip()
 
-    # se a chave for COR_PERCENT_R
-    elif data[0].strip() == 'COR_PERCENT_R':
-        # registra no dicionário
-        color[data[0].strip()] = data[1].strip()
+        # se a chave for ORDEM
+        elif data[0].strip() == 'ORDEM':
+            # registra no dicionário
+            color[data[0].strip()] = data[1].strip()
 
-    # se a chave for COR_PERCENT_G
-    elif data[0].strip() == 'COR_PERCENT_G':
-        # registra no dicionário
-        color[data[0].strip()] = data[1].strip()
+        # se a chave for COR_PERCENT_R
+        elif data[0].strip() == 'COR_PERCENT_R':
+            # registra no dicionário
+            color[data[0].strip()] = data[1].strip()
 
-    # se a chave for COR_PERCENT_B
-    elif data[0].strip() == 'COR_PERCENT_B':
-        # registra no dicionário
-        color[data[0].strip()] = data[1].strip()
+        # se a chave for COR_PERCENT_G
+        elif data[0].strip() == 'COR_PERCENT_G':
+            # registra no dicionário
+            color[data[0].strip()] = data[1].strip()
 
-    # se a chave for BLINK
-    elif data[0].strip() == 'BLINK':
-        # registra no dicionário
-        color[data[0].strip()] = data[1].strip()
+        # se a chave for COR_PERCENT_B
+        elif data[0].strip() == 'COR_PERCENT_B':
+            # registra no dicionário
+            color[data[0].strip()] = data[1].strip()
 
-    # se a chave for COR_BLINK
-    elif data[0].strip() == 'COR_BLINK':
-        # registra no dicionário
-        color[data[0].strip()] = data[1].strip()
+        # se a chave for BLINK
+        elif data[0].strip() == 'BLINK':
+            # registra no dicionário
+            color[data[0].strip()] = data[1].strip()
 
-    # se a chave for COR e o dicionário estiver totalmente populado
-    elif data[0].strip() == 'COR' and len(color) >= 5:
-        # insere a cor no array de cores
-        colors.append(color)
-        # reinicia o dicionário de cor
-        color = {}
+        # se a chave for COR_BLINK
+        elif data[0].strip() == 'COR_BLINK':
+            # registra no dicionário
+            color[data[0].strip()] = data[1].strip()
 
-
-# abrindo o arquivo CEPEL para leitura
-fileIn = open(os.path.join(__location__, 'corCEPEL.dat'))
-
-# inicializando o dicionário de cada cor
-color = {}
-
-# iterando sobre as linhas do arquivo
-for line in fileIn:
-
-    # separando os dados em um array chave valor
-    data = line.split('=')
-
-    # se a chave for ID
-    if data[0].strip() == 'ID':
-        # registra no dicionário
-        color[data[0].strip()] = data[1].strip()
-
-    # se a chave for ORDEM
-    elif data[0].strip() == 'ORDEM':
-        # registra no dicionário
-        color[data[0].strip()] = data[1].strip()
-
-    # se a chave for COR_PERCENT_R
-    elif data[0].strip() == 'COR_PERCENT_R':
-        # registra no dicionário
-        color[data[0].strip()] = data[1].strip()
-
-    # se a chave for COR_PERCENT_G
-    elif data[0].strip() == 'COR_PERCENT_G':
-        # registra no dicionário
-        color[data[0].strip()] = data[1].strip()
-
-    # se a chave for COR_PERCENT_B
-    elif data[0].strip() == 'COR_PERCENT_B':
-        # registra no dicionário
-        color[data[0].strip()] = data[1].strip()
-
-    # se a chave for BLINK
-    elif data[0].strip() == 'BLINK':
-        # registra no dicionário
-        color[data[0].strip()] = data[1].strip()
-
-    # se a chave for COR_BLINK
-    elif data[0].strip() == 'COR_BLINK':
-        # registra no dicionário
-        color[data[0].strip()] = data[1].strip()
-
-    # se a chave for COR e o dicionário estiver totalmente populado
-    elif data[0].strip() == 'COR' and len(color) >= 5:
-
-        # iterando sobre o array de cores
-        for c in colors:
-
-            # se o ID da cor já existir
-            if c.get('ID') == color['ID']:
-                # quebra o laço e não insere esta cor no array de cores
-                break
-
-        # se o loop varreu o array até o final sem quebrar
-        else:
+        # se a chave for COR e o dicionário estiver totalmente populado
+        elif data[0].strip() == 'COR' and len(color) >= 5:
             # insere a cor no array de cores
             colors.append(color)
             # reinicia o dicionário de cor
             color = {}
 
-# abrindo o arquivo MERGE para escrita
-fileOut = open(os.path.join(__location__, 'corMERGE.dat'), 'w+')
+    # fecha o arquivo
+    fileIn.close()
 
-# iterando sobre o array de cores
-for c in colors:
+# em caso de exceção de IO, envia mensagem e encerra o programa
+except FileNotFoundError as error:
+    print(f"Erro na abertura do arquivo corCHESF.dat: {error}")
+    exit()
 
-    # escrevendo no arquivo
-    fileOut.write('COR\n')
-    fileOut.write(f'ID = {c.get("ID")}\n')
-    fileOut.write(f'ORDEM = {c.get("ORDEM")}\n')
-    fileOut.write(f'COR_PERCENT_R = {c.get("COR_PERCENT_R")}\n')
-    fileOut.write(f'COR_PERCENT_G = {c.get("COR_PERCENT_G")}\n')
-    fileOut.write(f'COR_PERCENT_B = {c.get("COR_PERCENT_B")}\n')
+# em caso de outra exceção, envia mensagem, fecha o arquivo e encerra o programa
+except Exception as error:
+    print(f"Erro no processamento do arquivo corCHESF.dat: {error}")
+    fileIn.close()
+    exit()
 
-    # se a chave BLINK existir
-    if c.get("BLINK") is not None:
-        # escreve no arquivo
-        fileOut.write(f'BLINK = {c.get("BLINK")}\n')
+# tratando exceções
+try:
+    # abrindo o arquivo CEPEL para leitura
+    fileIn = open(os.path.join(__location__, 'corCEPEL.dat'))
 
-    # se a chave COR_BLINK existir
-    if c.get("COR_BLINK") is not None:
-        # escreve no arquivo
-        fileOut.write(f'COR_BLINK = {c.get("COR_BLINK")}\n')
+    # inicializando o dicionário de cada cor
+    color = {}
 
-    fileOut.write('\n')
+    # iterando sobre as linhas do arquivo
+    for line in fileIn:
 
-# envia mensagem de sucesso
-print('Concluído')
+        # separando os dados em um array chave valor
+        data = line.split('=')
+
+        # se a chave for ID
+        if data[0].strip() == 'ID':
+            # registra no dicionário
+            color[data[0].strip()] = data[1].strip()
+
+        # se a chave for ORDEM
+        elif data[0].strip() == 'ORDEM':
+            # registra no dicionário
+            color[data[0].strip()] = data[1].strip()
+
+        # se a chave for COR_PERCENT_R
+        elif data[0].strip() == 'COR_PERCENT_R':
+            # registra no dicionário
+            color[data[0].strip()] = data[1].strip()
+
+        # se a chave for COR_PERCENT_G
+        elif data[0].strip() == 'COR_PERCENT_G':
+            # registra no dicionário
+            color[data[0].strip()] = data[1].strip()
+
+        # se a chave for COR_PERCENT_B
+        elif data[0].strip() == 'COR_PERCENT_B':
+            # registra no dicionário
+            color[data[0].strip()] = data[1].strip()
+
+        # se a chave for BLINK
+        elif data[0].strip() == 'BLINK':
+            # registra no dicionário
+            color[data[0].strip()] = data[1].strip()
+
+        # se a chave for COR_BLINK
+        elif data[0].strip() == 'COR_BLINK':
+            # registra no dicionário
+            color[data[0].strip()] = data[1].strip()
+
+        # se a chave for COR e o dicionário estiver totalmente populado
+        elif data[0].strip() == 'COR' and len(color) >= 5:
+
+            # iterando sobre o array de cores
+            for c in colors:
+
+                # se o ID da cor já existir
+                if c.get('ID') == color['ID']:
+                    # quebra o laço e não insere esta cor no array de cores
+                    break
+
+            # se o loop varreu o array até o final sem quebrar
+            else:
+                # insere a cor no array de cores
+                colors.append(color)
+                # reinicia o dicionário de cor
+                color = {}
+
+    # fecha o arquivo
+    fileIn.close()
+
+# em caso de exceção de IO, envia mensagem e encerra o programa
+except FileNotFoundError as error:
+    print(f"Erro na abertura do arquivo corCHESF.dat: {error}")
+    exit()
+
+# em caso de outra exceção, envia mensagem, fecha o arquivo e encerra o programa
+except Exception as error:
+    print(f"Erro no processamento do arquivo corCHESF.dat: {error}")
+    fileIn.close()
+    exit()
+
+# tratando exceções
+try:
+
+    # abrindo o arquivo MERGE para escrita
+    fileOut = open(os.path.join(__location__, 'corMERGE.dat'), 'w+')
+
+    # iterando sobre o array de cores
+    for c in colors:
+
+        # escrevendo no arquivo
+        fileOut.write('COR\n')
+        fileOut.write(f'ID = {c.get("ID")}\n')
+        fileOut.write(f'ORDEM = {c.get("ORDEM")}\n')
+        fileOut.write(f'COR_PERCENT_R = {c.get("COR_PERCENT_R")}\n')
+        fileOut.write(f'COR_PERCENT_G = {c.get("COR_PERCENT_G")}\n')
+        fileOut.write(f'COR_PERCENT_B = {c.get("COR_PERCENT_B")}\n')
+
+        # se a chave BLINK existir
+        if c.get("BLINK") is not None:
+            # escreve no arquivo
+            fileOut.write(f'BLINK = {c.get("BLINK")}\n')
+
+        # se a chave COR_BLINK existir
+        if c.get("COR_BLINK") is not None:
+            # escreve no arquivo
+            fileOut.write(f'COR_BLINK = {c.get("COR_BLINK")}\n')
+
+        fileOut.write('\n')
+
+    # fecha o arquivo
+    fileOut.close()
+
+    # envia mensagem de sucesso
+    print('Concluído')
+
+# em caso de exceção, envia mensagem, fecha o arquivo e encerra o programa
+except Exception as error:
+    print(f"Erro no processamento do arquivo corMERGE.dat: {error}")
+    fileOut.close()
+    exit()
